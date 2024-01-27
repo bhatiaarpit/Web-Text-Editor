4a536797b1635d0f3e34274ff3d28ca76b5c206e
Arpit Bhatia <118626392+bhatiaarpit@users.noreply.github.com>
2024-01-27 14:35:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -115,3 +115,26 @@ const highlighterRemover = (className) => {
 };
 
 window.onload = initializer();
+
+//speech recognition
+click_to_convert.addEventListener('click',function(){
+  var speech = true;
+  window.SpeechRecognition = window.webkitSpeechRecognition;
+
+  const recognition = new SpeechRecognition();
+  recognition.interimResults = true;
+
+  recognition.addEventListener('result', e => {
+      const transcript = Array.from(e.results)
+          .map(result => result[0])
+          .map(result => result.transcript)
+          .join('')
+
+      document.getElementById("text-input").innerHTML = transcript;
+      console.log(transcript);
+  });
+  
+  if (speech == true) {
+      recognition.start();
+  }
+})
